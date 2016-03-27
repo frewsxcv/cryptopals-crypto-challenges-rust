247f313e2c75d16771f803df0afddd040c402ea5
Corey Farwell <coreyf@rwell.org>
2016-03-26 22:39:34 -0400

    Replace 'hex' crate with 'data-encoding' crate.

--- set-1/challenge-1/src/main.rs
+++ set-1/challenge-1/src/main.rs
@@ -1,18 +1,16 @@
-extern crate base64;
-extern crate hex;
+extern crate data_encoding;
 
-use hex::FromHex;
+use data_encoding::{hex, base64};
 
 static INPUT_HEX: &'static [u8] =
-    b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e\
-      6f7573206d757368726f6f6d";
+    b"49276D206B696C6C696E6720796F757220627261696E206C696B65206120706F69736F6E\
+      6F7573206D757368726F6F6D";
 
 static EXPECTED_B64: &'static [u8] =
     b"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 
 fn main() {
-    let input_hex_str = std::str::from_utf8(INPUT_HEX).unwrap();
-    let input_decoded = Vec::from_hex(input_hex_str).unwrap();
-    let input_base64 = &base64::u8en(&input_decoded).unwrap();
-    assert_eq!(EXPECTED_B64, &input_base64 as &[u8]);
+    let input_decoded = hex::decode(INPUT_HEX).unwrap();
+    let input_base64 = base64::encode(&input_decoded);
+    assert_eq!(EXPECTED_B64, input_base64.as_bytes());
 }
