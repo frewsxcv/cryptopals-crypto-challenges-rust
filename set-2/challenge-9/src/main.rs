12abeb008da825eade5ee87041e22d2955d67f2d
Corey Farwell <coreyf@rwell.org>
2017-10-18 22:42:29 -0400

    Follow instructions and actually implement PKCS #7 padding.

--- set-2/challenge-9/src/main.rs
+++ set-2/challenge-9/src/main.rs
@@ -1,13 +1,19 @@
 fn main() {
     assert_eq!(
-        pad(b"YELLOW SUBMARINE".to_vec(), 20, b'\x04'),
+        pad(b"YELLOW SUBMARINE".to_vec(), 20),
         b"YELLOW SUBMARINE\x04\x04\x04\x04"
-    )
+    );
+
+    assert_eq!(
+        pad(b"YELLOW".to_vec(), 6),
+        b"YELLOW\x06\x06\x06\x06\x06\x06"
+    );
 }
 
-fn pad(mut plaintext: Vec<u8>, block_len: usize, pad_val: u8) -> Vec<u8> {
+fn pad(mut plaintext: Vec<u8>, block_len: usize) -> Vec<u8> {
     let pad_len = block_len - (plaintext.len() % block_len);
     let new_len = plaintext.len() + pad_len;
-    plaintext.resize(new_len, pad_val);
+    assert!(pad_len < 256);
+    plaintext.resize(new_len, pad_len as u8);
     plaintext
 }
