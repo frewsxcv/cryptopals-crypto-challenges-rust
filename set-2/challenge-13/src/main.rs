138dd580f5a750df2cd0ed941a906cdde5bb499e
Corey Farwell <coreyf@rwell.org>
2018-04-17 09:31:05 +0900

    set 2, challenge 13, part 2

--- set-2/challenge-13/src/main.rs
+++ set-2/challenge-13/src/main.rs
@@ -1,10 +1,15 @@
 use std::collections::HashMap;
 
+type Profile = HashMap<String, String>;
+
 fn main() {
-    println!("{:?}", parse("foo=bar&baz=qux&zap=zazzle"));
+    let profile1 = parse("foo=bar&baz=qux&zap=zazzle");
+    println!("{:?}", profile1);
+    let profile2 = profile_for("foo@bar.com");
+    println!("{:?}", profile2);
 }
 
-fn parse(input: &str) -> HashMap<String, String> {
+fn parse(input: &str) -> Profile {
     input
         .split('&')
         .map(|pair| {
@@ -14,4 +19,12 @@ fn parse(input: &str) -> HashMap<String, String> {
             (key, value)
         })
         .collect()
+}
+
+fn profile_for(email: &str) -> Profile {
+    let mut map = HashMap::new();
+    map.insert("email".to_string(), email.to_string());
+    map.insert("uid".to_string(), "10".to_string());
+    map.insert("role".to_string(), "user".to_string());
+    map
 }
